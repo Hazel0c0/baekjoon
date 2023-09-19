84196cedba1b4a11e8c7c8982070e4b6f01cee9f
Hazel0c0 <secretwish@naver.com>
2023-09-19 23:35:03 +0900

    AplusB

---
+++ src/입출력/AplusB.java
@@ -0,0 +1,14 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class AplusB {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+    System.out.println(x+y);
+  }
+}
