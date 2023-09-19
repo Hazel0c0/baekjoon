d12ed35e1f43c291c166483d13a0a8c4d28f2dfd
Hazel0c0 <secretwish@naver.com>
2023-09-19 23:56:18 +0900

    year

---
+++ src/입출력/Year.java
@@ -0,0 +1,13 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class Year {
+  public static void main(String[] args) {
+    System.out.println(2541-1998); // 543
+    Scanner sc = new Scanner(System.in);
+    int y = sc.nextInt();
+
+    System.out.println(y-543);
+  }
+}
