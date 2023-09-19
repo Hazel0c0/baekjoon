9d938be08dfe4424302ecbaa8833de4076828449
Hazel0c0 <secretwish@naver.com>
2023-09-20 00:03:40 +0900

    dog

---
+++ src/입출력/Dog.java
@@ -0,0 +1,11 @@
+package 입출력;
+
+public class Dog {
+  public static void main(String[] args) {
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
