7edcf5dd329a956861c2c802a5bb041896039e04
Hazel0c0 <secretwish@naver.com>
2023-09-19 23:51:15 +0900

    AminusB

---
+++ src/입출력/AminusB.java
@@ -0,0 +1,13 @@
+package 입출력;
+
+import java.util.Scanner;
+public class AminusB {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+    System.out.println(x-y);
+  }
+}
