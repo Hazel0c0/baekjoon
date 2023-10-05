5a8599962ce7debe58cbee9905796c1eeb6fdfcf
Hazel0c0 <secretwish@naver.com>
2023-10-05 17:45:29 +0900

    일반수학 - 달팽이

---
+++ src/일반수학1/달팽이_시간초과.java
@@ -0,0 +1,25 @@
+package 일반수학1;
+
+import java.util.Scanner;
+
+public class 달팽이_시간초과 {
+  public static void main(String[] args) {
+    int V; // 나무 막대 높이
+    int A; // 낮에 올라갈 수 있는 높이
+    int B; // 밤에 내려오는 높이
+    // 정상에 올라가면 미끄러지지 않음. 정상에 도착 하는 시간은?
+
+    Scanner sc = new Scanner(System.in);
+    A = sc.nextInt();
+    B = sc.nextInt();
+    V = sc.nextInt();
+
+    int day = 0;
+    while (V!=0){
+      V=V-A;
+      if (V!=0) V=V+B;
+      day++;
+    }
+    System.out.println(day);
+  }
+}
