8eef1148fd066788eccdce1fa89860e87bf7caff
Evelyn <120666603+Hazel0c0@users.noreply.github.com>
2023-10-18 02:41:13 +0900

    [level unrated] Title: 문자열 섞기, Time: 12.63 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        char[] s1=str1.toCharArray();
+        char[] s2=str2.toCharArray();
+        for(int i=0; i<s1.length; i++){
+            answer+=s1[i];
+            answer+=s2[i];
+        }
+    
+        return answer;
+    }
+}
