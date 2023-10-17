1a28bb807cb3f462690296fe216d48b8a48c7ca6
Evelyn <120666603+Hazel0c0@users.noreply.github.com>
2023-10-18 03:13:00 +0900

    [level unrated] Title: 더 크게 합치기, Time: 7.69 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
         int aPlusb=Integer.parseInt(a+""+b);
         int bPlusa=Integer.parseInt(b+""+a);
-        if (aPlusb>bPlusa) {
-            answer=aPlusb;
-        } else if (aPlusb<bPlusa){
-            answer=bPlusa;
-        } else {
-            answer=aPlusb;
-        }
+        int answer = aPlusb;
+        answer=aPlusb>bPlusa?aPlusb:bPlusa;
+        
         return answer;
     }
 }
