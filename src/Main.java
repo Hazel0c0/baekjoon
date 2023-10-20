fe578db69749d00e5f1d59dacc13813aa2825129
Hazel0c0 <secretwish5@naver.com>
2023-10-20 12:24:30 +0900

    foreach

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     // 조건에 맞게 수열 변환하기 2
     int[] arr= {1, 2, 3, 100, 99, 98};
-    for (int i=0;i< arr.length;i++) {
-      if (arr[i] >= 50 && arr[i] % 2 == 0) arr[i] /= 2;
-      else if (arr[i] < 50 && arr[i] % 2 == 1) arr[i] = arr[i] * 2 + 1;
+    for (int val : arr) {
+      if (val >= 50 && val % 2 == 0) val /= 2;
+      else if (val < 50 && val % 2 == 1) val = val * 2 + 1;
     }
     System.out.println(Arrays.toString(arr));
 
