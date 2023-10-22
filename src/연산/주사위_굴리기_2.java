b75e6b1df7e0d09f1f090fe85a14b1b21e9219f7
Hazel0c0 <secretwish5@naver.com>
2023-10-22 21:29:44 +0900

    주사위 굴리기

---
+++ src/연산/주사위_굴리기_2.java
@@ -0,0 +1,27 @@
+package 연산;
+
+public class 주사위_굴리기_2 {
+
+  public static int solution(int a, int b, int c) {
+    /*
+    1~6 주사위, 3번 굴리기
+    셋다 다르면 ++
+    두개만 같으면 (++)*(^2+^2+^2)
+    셋다 (++)*(^2+^2+^2)*(^3+^3+^3)
+     */
+    int answer = 0;
+    if (a==b && b==c) {
+      return (int) (27*Math.pow(a,6));
+    } else if (a!=b && b!= c && a!= c){
+      return a+b+c;
+    } else {
+      return (a+b+c)*(a*a+b*b+c*c);
+    }
+  }
+
+  public static void main(String[] args) {
+    solution(2	,6	,1);
+    solution(5,3,3);
+    solution(4,4,4);
+  }
+}
