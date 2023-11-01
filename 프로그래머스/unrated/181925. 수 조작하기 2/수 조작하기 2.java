99452215b86aa9526a3f88468e2a3724c625ea9c
Evelyn <120666603+Hazel0c0@users.noreply.github.com>
2023-11-01 16:42:51 +0900

    [level unrated] Title: 수 조작하기 2, Time: 727.72 ms, Memory: 372 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -4,21 +4,14 @@ import java.util.Queue;
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        Queue<Integer> queue = new LinkedList<>();
-        for(int n:numLog){
-            queue.add(n);
-        }
-        int before=queue.poll(); // 0
-        while (true){
-            int after=queue.poll(); // 1
-            switch (after-before){ // 1
-                case 1: answer+="w"; break;
-                case -1: answer+="s"; break;
-                case 10: answer+="d"; break;
-                case -10: answer+="a"; break;
+        for(int i=1; i<numLog.length; i++){
+            switch(numLog[i-1] - numLog[i]){
+                case -1 : answer+='w'; break;
+                case 1 : answer+='s'; break;
+                case -10 : answer+='d'; break;
+                case 10 : answer+='a'; break;
             }
-            before+=(after-before); // 1
-            if (queue.isEmpty()) return answer;
         }
+        return answer;
     }
 }
